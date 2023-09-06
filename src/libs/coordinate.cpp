6fa6cf05a1365fcc1d95c93166416bbfbf06c2d6
chongwish <chongwish@t440s.local>
2023-09-06 18:51:04 +0800

    coordinate generalized

--- src/libs/coordinate.cpp
+++ src/libs/coordinate.cpp
@@ -1,17 +1,1 @@
 #include "coordinate.hpp"
-
-inline void Coordinate::mark(uint16_t x, uint16_t y, uint16_t color) {
-    points[height * y + x] = color;
-}
-
-inline uint16_t Coordinate::getMark(uint16_t x, uint16_t y) {
-    return points[height * y + x];
-}
-
-Coordinate::Coordinate(uint16_t width, uint16_t height) : width(width), height(height) {
-    points = new uint16_t[width * height];
-}
-
-Coordinate::~Coordinate() {
-    delete[] points;
-}
--- src/libs/coordinate.hpp
+++ src/libs/coordinate.hpp
@@ -4,16 +4,37 @@
 
 #define AIRDUNIO_LIB_COORDINATE 1
 
+template<typename T> 
 class Coordinate {
     public:
         Coordinate(uint16_t width, uint16_t height);
         ~Coordinate();
+        virtual void mark(uint16_t x, uint16_t y, T color);
+        virtual T getMark(uint16_t x, uint16_t y);
     protected:
-        virtual void mark(uint16_t x, uint16_t y, uint16_t color);
-        virtual uint16_t getMark(uint16_t x, uint16_t y);
         uint16_t width;
         uint16_t height;
-        uint16_t *points;
+        T *points;
 };
+
+template<typename T>
+inline void Coordinate<T>::mark(uint16_t x, uint16_t y, T color) {
+    points[width * y + x] = color;
+}
+
+template<typename T>
+inline T Coordinate<T>::getMark(uint16_t x, uint16_t y) {
+    return points[width * y + x];
+}
+
+template<typename T>
+Coordinate<T>::Coordinate(uint16_t width, uint16_t height) : width(width), height(height) {
+    points = new T[width * height];
+}
+
+template<typename T>
+Coordinate<T>::~Coordinate() {
+    delete[] points;
+}
 
 #endif
