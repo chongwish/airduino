723a3cff1caa16b3209ea153a005c0a1f1ab1392
chongwish <chongwish@t440s.local>
2023-09-06 18:48:46 +0800

    display interface rework

--- src/libs/display.hpp
+++ src/libs/display.hpp
@@ -4,11 +4,32 @@
 
 #define AIRDUNIO_LIB_DISPLAY 1
 
+template<typename T>
 class Display {
     public:
+        Display(uint16_t width, uint16_t height);
         virtual void refresh() = 0;
-        virtual void fillPoint(uint16_t x, uint16_t y, uint16_t color) = 0;
-        virtual void fillBlock(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color) = 0;
+        virtual void fillPoint(uint16_t x, uint16_t y, T color) = 0;
+        virtual void fillBlock(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, T color) = 0;
+        uint16_t getWidth();
+        uint16_t getHeight();
+    protected:
+        uint16_t width;
+        uint16_t height;
 };
+
+template<typename T>
+Display<T>::Display(uint16_t width, uint16_t height) : width(width), height(height) {
+}
+
+template<typename T>
+inline uint16_t Display<T>::getWidth() {
+    return width;
+}
+
+template<typename T>
+inline uint16_t Display<T>::getHeight() {
+    return height;
+}
 
 #endif
